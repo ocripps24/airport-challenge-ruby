45485e2f53fbce96481d55920aa199d1f6d8c073
Oliver Cripps <ollycripps@gmail.com>
2019-07-01 17:45:15 +0100

    Add takeoff method

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -2,17 +2,19 @@ require 'airport'
 require 'plane'
 
 describe 'User Stories' do
-  it 'so planes land at airports, instruct a plan to land' do
+  it 'so planes land at airports, instruct a plane to land' do
     airport = Airport.new
     plane = Plane.new
     expect { airport.land(plane) }.not_to raise_error
   end
+
+  it 'so planes take off, instruct a plane to take off' do
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.take_off(plane) }.not_to raise_error
+  end
 end
 
-# As an air traffic controller
-# So I can get passengers to a destination
-# I want to instruct a plane to land at an airport
-#
 # As an air traffic controller
 # So I can get passengers on the way to their destination
 # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
